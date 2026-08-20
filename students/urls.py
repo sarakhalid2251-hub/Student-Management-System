03e111bb559f5d588f3d43cda51efb00a9d9c712
sarakhalid2251-hub <sarakhalid2251@gmail.com>
2026-08-19 17:25:04 -0700

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
     path('add-marks/', views.add_marks, name='add_marks'),
     path('add-attendance/', views.add_attendance, name='add_attendance'),
-]
+]              
